4652f449af24af0c6659b8fcf75a9bf7fe8fa56f
Peter Bennion <pcbennion@gmail.com>
2025-09-26 10:14:45 -0400

    Prepare for release

--- conanfile.py
+++ conanfile.py
@@ -7,16 +7,14 @@ from conan.tools.scm import Git
 
 class OpensslEvpWasm(ConanFile):
     name = "openssl-evp-wasm"
-    license = "All Rights Reserved by Qrypt"
-    author = "Qrypt Inc info@qrypt.com"
+    license = "MIT"
+    author = "Peter Bennion pcbennion@gmail.com, Qrypt Inc info@qrypt.com"
     url = "https://github.com/pcbennion/openssl-evp-wasm"
     description = ("Webassembly bindings for the OpenSSL EVP crypto interface.")
-    topics = ("Qrypt", "cryptography", "interface")
+    topics = ("openssl", "webassembly", "cryptography", "interface")
     settings = "os", "compiler", "build_type", "arch"
     options = {"modularize": [True, False]}
     default_options = {"modularize": False}
-
-    _in_source = False
 
     def set_version(self):
         # If no version has been set, set it to the current git branch name
